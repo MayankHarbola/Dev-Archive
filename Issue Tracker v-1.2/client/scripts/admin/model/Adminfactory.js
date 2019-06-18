7f805932d9aff7beb6a0e1d09a41a8d1f26c74da
Mayank Harbola <36039776+MayankHarbola@users.noreply.github.com>
2019-06-18 08:44:10 +0530

    Added Tester Issue Creating page
    Added Tester Issue Creating page

---
+++ Issue Tracker v-1.2/client/scripts/admin/model/Adminfactory.js
@@ -0,0 +1,53 @@
+app.factory('adminfact',function($http,$q,USER_LIST,ADD_USER,EDIT_USER,DELETE_USER){
+return{
+    userlist(){
+        let defer = $q.defer();
+        $http.get(USER_LIST).then((data)=>{
+            defer.resolve(data);
+        },(error)=>{
+            defer.reject(error);
+        })
+        return defer.promise;
+    },
+
+    add(data){
+        let defer = $q.defer();
+
+        $http.post(ADD_USER,JSON.stringify(data)).then((data)=>{
+            defer.resolve(data);   
+        },(error)=>{
+            defer.reject(error);
+        });
+
+        return defer.promise;
+    },
+
+    edit(data){
+        let defer = $q.defer();
+
+        $http.post(EDIT_USER,JSON.stringify(data)).then((data)=>{
+            defer.resolve(data);   
+        },(error)=>{
+            defer.reject(error);
+        });
+
+        return defer.promise;
+    },
+    
+    delete(data){
+        let defer = $q.defer();
+
+        $http.post(DELETE_USER,JSON.stringify(data)).then((data)=>{
+            defer.resolve(data);   
+        },(error)=>{
+            defer.reject(error);
+        });
+
+        return defer.promise;
+    },
+
+    
+
+
+}
+})
