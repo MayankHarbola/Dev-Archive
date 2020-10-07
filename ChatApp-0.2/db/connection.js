8f40a93df52d624f9330607592bf3367d4cf6574
Abhishek Sindhwani <32593892+abhi99sind@users.noreply.github.com>
2020-10-07 12:45:07 +0530

    Update connection.js

--- ChatApp-0.2/db/connection.js
+++ ChatApp-0.2/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const env = require("../utils/env");
-
+//Loda Harbola
 mongoose.connect(env.mongo);
 mongoose.connection.on('open',()=>{
     console.log('ConnectionOpen');
